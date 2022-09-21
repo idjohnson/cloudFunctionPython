0ecd1f36f929dd7863eedd00c35440d96249c309
Isaac Johnson <isaac.johnson@gmail.com>
2022-09-21 06:00:30 -0500

    Initial (redo)

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import apprise
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    apobj = apprise.Apprise()
+    apobj.add('mailto://tristan.cormac.moriarty:********@gmail.com/?to=isaac.johnson@gmail.com')
+    apobj.notify(
+        body='Notified by Cloud Run Function',
+        title='From Python Cloud Run',
+    )
+    return "Hello hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
