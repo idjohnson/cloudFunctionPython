77dc5cae46dab76f9dfbdce3f29530bba980533e
Isaac Johnson <isaac.johnson@gmail.com>
2022-09-24 19:09:05 -0500

    attempt testing

--- test_main.py
+++ test_main.py
@@ -4,5 +4,5 @@ import json
 def test_main(app, client):
     res = client.get('/')
     assert res.status_code == 200
-    expected = "Hello hello Code!"
+    expected = "Hello hello World!"
     assert expected == res.get_data(as_text=True)
