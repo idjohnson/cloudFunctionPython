a7eab476106f6ed1e1dd792b49de3021bdb8d612
Isaac Johnson <isaac.johnson@gmail.com>
2022-10-05 19:17:19 -0500

    add another route

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ def hello_world():
     )
     return "Hello hello {}!".format(name)
 
+@app.route("/asdf1234")
+def hello_world2():
+    name = os.environ.get("NAME", "World")
+    apobj = apprise.Apprise()
+    apobj.add('sendgrid://SENDGRIDTOKENHERE:isaac@freshbrewed.science/isaac.johnson@gmail.com')
+    apobj.add('msteams://MSTEAMSTOKENHERE')
+    apobj.notify(
+        body='Notified by Cloud Run Function',
+        title='From Python Cloud Run',
+    )
+    return "Hello hello {}!".format(name)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
